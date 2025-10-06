833573d5443ef0f094ddd1c7e734a564dedcab5e
Hp <Hp@Sanjay>
2025-10-06 19:37:36 +0530

    assignment

--- assignments/src/com/tnsif/assignmentone/Mainclass.java
+++ assignments/src/com/tnsif/assignmentone/Mainclass.java
@@ -1,5 +1,13 @@
 package com.tnsif.assignmentone;
 
 public class Mainclass {
+	public static void main(String[] args) {
+		Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+		// TODO Auto-generated method stub
+
+	}
+
 
 }
--- assignments/src/com/tnsif/assignmentone/Student.java
+++ assignments/src/com/tnsif/assignmentone/Student.java
@@ -1,5 +1,8 @@
 package com.tnsif.assignmentone;
 
 public class Student {
+	 Student() {
+	        System.out.println("Student object is created");
+	    }
 
 }
